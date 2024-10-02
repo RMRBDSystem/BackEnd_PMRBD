8a349ee50ca263619f153683e10b0b6320e13082
Anh Tuan <vodanh36824@gmail.com>
2024-10-02 12:04:05 +0700

    Add last 9 file  DAO in dataAccess

---
+++ DataAccess/RecipeDAO.cs
@@ -0,0 +1,54 @@
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class RecipeDAO : SingletonBase<RecipeDAO>
+    {
+        public async Task<IEnumerable<Recipe>> GetAllRecipes() => await _context.Recipes.ToListAsync();
+
+        public async Task<Recipe> GetRecipeById(int id)
+        {
+            var recipe = await _context.Recipes
+                .Where(c => c.RecipeId == id)
+                .FirstOrDefaultAsync();
+            if (recipe == null) return null;
+            return recipe;
+        }
+
+        public async Task Add(Recipe recipe)
+        {
+            _context.Recipes.AddAsync(recipe);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Recipe recipe)
+        {
+            var existingItem = await GetRecipeById(recipe.RecipeId);
+            if (existingItem != null)
+            {
+                _context.Entry(existingItem).CurrentValues.SetValues(recipe);
+            }
+            else
+            {
+                _context.Recipes.Add(recipe);
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var recipe = await GetRecipeById(id);
+            if (recipe != null)
+            {
+                _context.Recipes.Remove(recipe);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ DataAccess/RecipeRateDAO.cs
@@ -0,0 +1,54 @@
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class RecipeRateDAO : SingletonBase<RecipeRateDAO>
+    {
+        public async Task<IEnumerable<RecipeRate>> GetAllRecipeRates() => await _context.RecipeRates.ToListAsync();
+
+        public async Task<RecipeRate> GetRecipeRateById(int id)
+        {
+            var reciperate = await _context.RecipeRates
+                .Where(c => c.RecipeId == id)
+                .FirstOrDefaultAsync();
+            if (reciperate == null) return null;
+            return reciperate;
+        }
+
+        public async Task Add(RecipeRate reciperate)
+        {
+            _context.RecipeRates.AddAsync(reciperate);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(RecipeRate reciperate)
+        {
+            var existingItem = await GetRecipeRateById(reciperate.RecipeId);
+            if (existingItem != null)
+            {
+                _context.Entry(existingItem).CurrentValues.SetValues(reciperate);
+            }
+            else
+            {
+                _context.RecipeRates.Add(reciperate);
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var reciperate = await GetRecipeRateById(id);
+            if (reciperate != null)
+            {
+                _context.RecipeRates.Remove(reciperate);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
