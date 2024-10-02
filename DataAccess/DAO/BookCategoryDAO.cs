8f7bc830cad9b8a02836084f0d256330395c1b7c
Voquoc <voquoc2002@gmail.com>
2024-10-02 20:36:13 +0700

    10 FirstDAO

---
+++ DataAccess/DAO/BookCategoryDAO.cs
@@ -0,0 +1,66 @@
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class BookCategoryDAO : SingletonBase<BookCategoryDAO>
+    {
+        public async Task<IEnumerable<BookCategory>> GetAllBookCategories()
+        {
+            try
+            {
+                return await _context.BookCategories.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve book categories", ex);
+            }
+        }
+
+        public async Task<BookCategory?> GetBookCategoryById(int id)
+        {
+            try
+            {
+                return await _context.BookCategories.FirstOrDefaultAsync(x => x.CategoryId == id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve book category by id", ex);
+            }
+        }
+
+        public async Task SaveBookCategory(BookCategory bookCategory)
+        {
+            try
+            {
+                if (bookCategory != null)
+                {
+                    await _context.BookCategories.AddAsync(bookCategory);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to save book category", ex);
+            }
+        }
+
+        public async Task UpdateBookCategory(BookCategory bookCategory)
+        {
+            try
+            {
+                _context.Entry(bookCategory).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to update book category", ex);
+            }
+        }
+    }
+}
---
+++ DataAccess/DAO/BookShelfDAO.cs
@@ -0,0 +1,69 @@
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class BookShelfDAO : SingletonBase<BookShelfDAO>
+    {
+        public async Task<IEnumerable<BookShelf>> GetAllBookShelfByCustomerId(int id)
+        {
+            try
+            {
+                return await _context.BookShelves.Where(x => x.CustomerId == id).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve bookshelfs", ex);
+            }
+        }
+
+        public async Task<BookShelf?> GetBookShelfByEBookIdAndCustomerId(int EBookid, int Customerid)
+        {
+            try
+            {
+                return await _context.BookShelves.FirstOrDefaultAsync(x => x.EbookId == EBookid && x.CustomerId == Customerid);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve book by id", ex);
+            }
+        }
+
+        public async Task SaveBookShelf(BookShelf bookshelf)
+        {
+            try
+            {
+                if (bookshelf != null)
+                {
+                    await _context.BookShelves.AddAsync(bookshelf);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to save bookshelf", ex);
+            }
+        }
+
+        public async Task UpdateBookShelf(BookShelf bookshelf)
+        {
+            try
+            {
+                if (bookshelf != null)
+                {
+                    _context.Entry(bookshelf).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                }              
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to update bookshelf", ex);
+            }
+        }
+    }
+}
