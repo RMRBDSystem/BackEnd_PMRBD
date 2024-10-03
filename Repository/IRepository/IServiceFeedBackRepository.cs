46a18ee415b2e3f0418feb237a71decc56688809
Anh Tuan <vodanh36824@gmail.com>
2024-10-03 20:13:58 +0700

    Add ServiceFeedback

---
+++ Repository/IRepository/IServiceFeedBackRepository.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.IRepository
+{
+    public interface IServiceFeedBackRepository
+    {
+        Task<IEnumerable<ServiceFeedBack>> GetAllServiceFeedBacks();
+        Task<ServiceFeedBack> GetServiceFeedBackById(int id);
+        Task AddServiceFeedBack(ServiceFeedBack feedBack);
+        Task UpdateServiceFeedBack(ServiceFeedBack feedBack);
+    }
+}
---
+++ Repository/Repository/ServiceFeedBackRepository.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObject.Models;
+using DataAccess;
+using DataAccess.DAO;
+using Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class ServiceFeedBackRepository : IServiceFeedBackRepository
+    {
+        public async Task<IEnumerable<ServiceFeedBack>> GetAllServiceFeedBacks() => await ServiceFeedBackDAO.Instance.GetAllServiceFeedBacks();
+        public async Task<ServiceFeedBack> GetServiceFeedBackById(int id) => await ServiceFeedBackDAO.Instance.GetServiceFeedBackById(id);
+        public async Task AddServiceFeedBack(ServiceFeedBack ServiceFeedBack) => await ServiceFeedBackDAO.Instance.Add(ServiceFeedBack);
+        public async Task UpdateServiceFeedBack(ServiceFeedBack ServiceFeedBack) => await ServiceFeedBackDAO.Instance.Update(ServiceFeedBack);
+    }
+}
