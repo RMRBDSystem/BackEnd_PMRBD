7cf7becee623baba317bd0e5733ca29de49e57eb
Voquoc <voquoc2002@gmail.com>
2024-10-06 11:23:51 +0700

    Add RMRBDClient MVC + ApiService

---
+++ RMRBDClient/Models/ApiSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace RMRBD_Client.Service
+{
+    public class ApiSettings
+    {
+        public string BookUrl { get; set; }
+        public string BookCategoryUrl { get; set; }
+        public string BookOrderUrl { get; set; }
+        public string BookOrderDetailUrl { get; set; }
+        public string BookRateUrl { get; set; }
+        public string BookShelfUrl { get; set; }
+        public string CommentUrl { get; set; }
+        public string CustomerUrl { get; set; }
+        public string EbookUrl { get; set; }
+        public string EmployeeUrl { get; set; }
+        public string EmployeeTypeUrl { get; set; }
+        public string ImageUrl { get; set; }
+        public string NotificationUrl { get; set; }
+        public string PersonalRecipeUrl { get; set; }
+        public string RecipeUrl { get; set; }
+        public string RecipeRateUrl { get; set; }
+        public string ServiceFeedBackUrl { get; set; }
+        public string TagUrl { get; set; }
+        public string TransactionUrl { get; set; }
+        
+    }
+}
