ae9e31b57a11da55bff90a7ddba28006055d077b
Anh Tuan <vodanh36824@gmail.com>
2024-11-14 13:06:17 +0700

    thêm khóa chính bookrate

--- BussinessObject/Models/BookRate.cs
+++ BussinessObject/Models/BookRate.cs
@@ -1,20 +1,22 @@
-﻿using System;
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Text.Json.Serialization;
-
-namespace BusinessObject.Models;
-
-public partial class BookRate
+using System.Threading.Tasks;
+namespace BusinessObject.Models
 {
-    public int RatePoint { get; set; }
-
-    public int CustomerId { get; set; }
-
-    public int BookId { get; set; }
-
-    [JsonIgnore]
-    public virtual Book? Book { get; set; }
-
-    [JsonIgnore]
-    public virtual Account? Customer { get; set; }
+    [PrimaryKey(nameof(BookId), nameof(CustomerId))]
+    public partial class BookRate
+    {
+        public int RatePoint { get; set; }
+        public int CustomerId { get; set; }
+        public int BookId { get; set; }
+        [JsonIgnore]
+        public virtual Book? Book { get; set; }
+        [JsonIgnore]
+        public virtual Account? Customer { get; set; }
+    }
 }
