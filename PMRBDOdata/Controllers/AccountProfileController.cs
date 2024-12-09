0fd34229ffe3cb3b8077ad9b4ec673dad0c5e772
phamngockhanh <ngockhanhpham8a@gmail.com>
2024-12-09 19:31:26 +0700

    "Update send email, block user function"

--- PMRBDOdata/Controllers/AccountProfileController.cs
+++ PMRBDOdata/Controllers/AccountProfileController.cs
@@ -1,20 +1,15 @@
-﻿using System.Net.Sockets;
-using BusinessObject.Models;
+﻿using BusinessObject.Models;
 using Firebase.Auth;
 using Firebase.Storage;
-using Microsoft.AspNetCore.Http;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Formatter;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OData.Edm;
 using MimeKit;
 using Repository.IRepository;
 using Repository.Repository;
-using MailKit.Net.Smtp;
-using static System.Net.Mime.MediaTypeNames;
-using MailKit.Security;
 
 namespace PMRBDOdata.Controllers
 {
@@ -42,7 +37,7 @@ namespace PMRBDOdata.Controllers
             message.To.Add(new MailboxAddress("", toEmail));
             message.Subject = subject;
 
-            message.Body = new TextPart("plain")
+            message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
             {
                 Text = body
             };
@@ -54,8 +49,8 @@ namespace PMRBDOdata.Controllers
                     // Kết nối đến Gmail SMTP server với bảo mật TLS
                     await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
 
-                    // Sử dụng mật khẩu ứng dụng thay vì mật khẩu chính
-                    await client.AuthenticateAsync("ngockhanhpham8a@gmail.com", "iukm cdoc qkwx wmqu"); 
+                    // Sử dụng mật khẩu ứng dụng
+                    await client.AuthenticateAsync("ngockhanhpham8a@gmail.com", "iukm cdoc qkwx wmqu");
 
                     await client.SendAsync(message); // Gửi email
                     await client.DisconnectAsync(true); // Ngắt kết nối sau khi gửi
@@ -119,23 +114,47 @@ namespace PMRBDOdata.Controllers
             }
             accountProfile.AccountId = accountProfileToUpdate.AccountId;
             await accountProfileRepository.UpdateAccountProfile(accountProfile);
+
+            var loginUrl = "https://localhost:5173/login";
             if (accountProfile.Status == 0)
             {
                 var userEmail = accountProfileToUpdate.Account.Email;
-                var subject = "Yêu cầu gửi lại thông tin tài khoản";
-                var body = $"Chào {accountProfileToUpdate.Account.UserName},\n\nHồ sơ của bạn đã bị từ chối vì chưa đầy đủ hoặc không hợp lệ. Vui lòng kiểm tra và gửi lại form.";
-
+                var subject = "Yêu cầu bổ sung thông tin tài khoản";
+                var body = $@"
+                <html>
+                    <body>
+                        <p>Chào <strong>{accountProfileToUpdate.Account.UserName}</strong>,</p>
+                        <br>
+                        <p>Cảm ơn bạn đã đăng ký tài khoản trên hệ thống của chúng tôi. Tuy nhiên, hồ sơ của bạn chưa được duyệt vì một số thông tin còn thiếu hoặc không hợp lệ.</p>
+                        <p>Để đảm bảo tài khoản được kích hoạt thành công, vui lòng kiểm tra lại thông tin và gửi lại biểu mẫu đăng ký của bạn.</p>
+                        <p>Nếu cần hỗ trợ thêm, bạn có thể liên hệ với chúng tôi qua email <a href='mailto:RMRBDSystem@gmail.com'>RMRBDSystem@gmail.com</a> hoặc số hotline <strong>1800-123-456</strong>.</p>
+                        <br>
+                        <p>Trân trọng,<br>
+                        Đội ngũ Hỗ trợ Khách hàng.</p>
+                    </body>
+                </html>";
                 // Gửi email thông báo
                 await SendEmailAsync(userEmail, subject, body);
-
-                return BadRequest("Hồ sơ bị từ chối. Vui lòng kiểm tra và gửi lại.");
             }
 
             if (accountProfile.Status == 1)
             {
                 var userEmail = accountProfileToUpdate.Account.Email;
-                var subject = "Thông báo thay đổi trạng thái tài khoản";
-                var body = $"Chào {accountProfileToUpdate.Account.UserName},\n\nTrạng thái tài khoản của bạn đã được thay đổi thành công!";
+                var subject = "Chúc mừng! Tài khoản của bạn đã được duyệt";
+                var body = $@"
+                <html>
+                  <body>
+                      <p>Chào <strong>{accountProfileToUpdate.Account.UserName}</strong>,</p>
+                      <br>
+                      <p>Chúc mừng! Tài khoản của bạn trên hệ thống của chúng tôi đã được duyệt thành công. Bạn có thể bắt đầu sử dụng tất cả các tính năng mà chúng tôi cung cấp.</p>
+                      <p>Hãy đăng nhập và khám phá ngay tại: <a href='{loginUrl}'>Đăng nhập ngay</a>.</p>
+                      <p>Nếu bạn gặp bất kỳ vấn đề nào trong quá trình sử dụng, vui lòng liên hệ với đội ngũ hỗ trợ của chúng tôi.</p>
+                      <p>Cảm ơn bạn đã tin tưởng sử dụng dịch vụ của chúng tôi!</p>
+                      <br>
+                      <p>Trân trọng,<br>
+                      Đội ngũ Hỗ trợ Khách hàng.</p>
+                  </body>
+                </html>";
 
                 // Gửi email thông báo
                 await SendEmailAsync(userEmail, subject, body);
--- PMRBDOdata/Controllers/LoginController.cs
+++ PMRBDOdata/Controllers/LoginController.cs
@@ -1,15 +1,10 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+﻿using System.Text;
 using BusinessObject.Models;
-using DataAccess;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using DataAccess.DAO;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
-using Microsoft.IdentityModel.Tokens;
-using DataAccess.DAO;
 
 namespace PMRBDOdata.Controllers
 {
@@ -62,16 +57,10 @@ namespace PMRBDOdata.Controllers
         [HttpPost]
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
-            
+
             // Check GoogleID in Employee table through DAO
             var checkAccount = await AccountDAO.Instance.GetAccountByGoogleId(request.GoogleId);
-
-            if (checkAccount != null && checkAccount.AccountStatus == 0)
-
-            {
-                return Unauthorized(new { message = "Tài khoản của bạn đã bị khóa!" });
-            }
-            if (checkAccount != null && checkAccount.AccountStatus == 1)
+            if (checkAccount != null)
             {
                 // Gán role dựa trên EmployeeTypeId
                 string role;
@@ -127,7 +116,7 @@ namespace PMRBDOdata.Controllers
 
             return GetSessionInfo();
         }
-        
+
         [HttpPost("logout")]
         public async Task<IActionResult> Logout()
         {
